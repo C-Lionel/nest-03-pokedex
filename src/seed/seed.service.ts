0b25118d0a1a1b4ea216c04679126c422915ed22
lionel <lio2014vt@gmail.com>
2024-10-21 22:40:45 -0300

    updated antes de la tarea: PC

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -9,12 +9,15 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
-      const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+      const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
       data.results.forEach( ({name, url}) => {
+
         const segments = url.split('/');
         const no = +segments[segments.length - 2]; 
+        
         console.log({name, no})
+
       } )
 
       return data.results;
